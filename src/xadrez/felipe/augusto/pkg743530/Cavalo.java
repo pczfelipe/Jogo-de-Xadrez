46288d8af34841cb30c78ec4bc4a3d5fdd92f9ac
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2020-10-25 14:44:18 -0300

    feat: Implementação classe Cavalo

--- src/xadrez/felipe/augusto/pkg743530/Cavalo.java
+++ src/xadrez/felipe/augusto/pkg743530/Cavalo.java
@@ -5,5 +5,35 @@ package xadrez.felipe.augusto.pkg743530;
  * @author Felipe Augusto - 743530
  */
 public class Cavalo {
+    //Atributos
+    private int cor; //0 para brancas, 1 para pretas
+    private boolean inGame;
     
+    //Metodos
+    public Cavalo(int cor) {
+        this.cor = cor;
+        this.inGame = true;
+    }
+
+    public int getColor() {
+        return cor;
+    }
+    
+    public String desenho(){
+        if(getColor() == 0) //peca branca
+            return("c");
+        else
+            return("C");
+    }
+    
+    public boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino){
+        switch (Math.abs(linhaDestino - linhaOrigem)) {
+            case 2:
+                return Math.abs(colunaDestino - colunaOrigem) == 1;
+            case 1:
+                return Math.abs(colunaDestino - colunaOrigem) == 2;
+            default:
+                return false;
+        }
+    }
 }
