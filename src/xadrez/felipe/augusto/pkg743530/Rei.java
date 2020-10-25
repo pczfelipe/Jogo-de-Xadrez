32f21204d58165dba2c226412032b997d6393525
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2020-10-25 14:46:02 -0300

    feat: Implementação classe Rei

--- src/xadrez/felipe/augusto/pkg743530/Rei.java
+++ src/xadrez/felipe/augusto/pkg743530/Rei.java
@@ -5,5 +5,28 @@ package xadrez.felipe.augusto.pkg743530;
  * @author Felipe Augusto - 743530
  */
 public class Rei {
+    //Atributos
+    private int cor; //0 para brancas, 1 para pretas
+    private boolean inGame;
     
+    //Metodos
+    public Rei(int cor) {
+        this.cor = cor;
+        this.inGame = true;
+    }
+
+    public int getColor() {
+        return cor;
+    }
+    
+    public String desenho(){
+        if(getColor() == 0) //peca branca
+            return("r");
+        else
+            return("R");
+    }
+    
+    public boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino){
+        return Math.abs(linhaDestino - linhaOrigem) <= 1 && Math.abs(colunaDestino - colunaOrigem) <= 1;
+    }
 }
