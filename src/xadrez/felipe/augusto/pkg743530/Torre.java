9747897100c6d9da8629453ff77524195a9239f8
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2020-10-25 14:49:16 -0300

    feat: Implementação da classe Torre

--- src/xadrez/felipe/augusto/pkg743530/Torre.java
+++ src/xadrez/felipe/augusto/pkg743530/Torre.java
@@ -5,5 +5,28 @@ package xadrez.felipe.augusto.pkg743530;
  * @author Felipe Augusto - 743530
  */
 public class Torre {
+    //Atributos
+    private int cor; //0 para brancas, 1 para pretas
+    private boolean inGame;
     
+    //Metodos
+    public Torre(int cor) {
+        this.cor = cor;
+        this.inGame = true;
+    }
+
+    public int getColor() {
+        return cor;
+    }
+    
+    public String desenho(){
+        if(getColor() == 0) //peca branca
+            return("t");
+        else
+            return("T");
+    }
+    
+    public boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino){
+        return (linhaOrigem == linhaDestino && colunaOrigem != colunaDestino) || (linhaOrigem != linhaDestino) && (colunaOrigem == colunaDestino);
+    }  
 }
