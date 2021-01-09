319574964edd166a75f6358e4bae6e2182019214
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2021-01-08 22:52:02 -0300

    feat: Posicao completo

--- src/xadrez/felipe/augusto/pkg743530/Posicao.java
+++ src/xadrez/felipe/augusto/pkg743530/Posicao.java
@@ -6,27 +6,49 @@ package xadrez.felipe.augusto.pkg743530;
  */
 public class Posicao {
     //Atributos
-    private String cor; //00 para branco, 11 para Preto
-    private int linha; //de 1 a 8
-    private String coluna; //de "a" a "h", ou seja, de 1 a 8
+    private final String cor; //00 para branco, 11 para Preto
+    private final int linha; //de 1 a 8
+    private final String coluna; //de "A" a "H", ou seja, de 1 a 8
     private Peca ocupante; //é um objeto Peça, posteriormente sera implementado
     
     //Metodos Especiais
+    /**
+    *Construtor de tabuleiro, responsável por criar as posições e inserir as peças criadas nas posicoes iniciais do xadrex
+    * 
+    * @param cor Cor da posicao no tabuleiro
+    * @param linha Linha no tabuleiro
+    * @param coluna Coluna no tabuleiro
+    */
     public Posicao(String cor, int linha, String coluna) {
         this.cor = cor;
         this.linha = linha;
         this.coluna = coluna;
         this.ocupante = null;
     }
-    
+
+    /**
+     *Retorna a cor da posicao no tabuleiro
+     * 
+     * @return Retorna String que representa cor
+     */
     public String isCor() {
         return cor;
     }
 
+     /**
+     *Retorna a peca na posicao no tabuleiro
+     * 
+     * @return Retorna um objeto peca 
+     */
     public Peca getOcupante() {
         return ocupante;
     }
 
+     /**
+     *Responsavel por colocar uma nova peca na nova posicao
+     * 
+     * @param ocupante Recebe um objeto peca 
+     */
     public void setOcupante(Peca ocupante) {
         this.ocupante = ocupante;
     }
