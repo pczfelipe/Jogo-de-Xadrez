160f06507c55e44caaef42bc0b53a02b42247557
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2021-01-08 22:54:20 -0300

    feat: Jogador completo

--- src/xadrez/felipe/augusto/pkg743530/Jogador.java
+++ src/xadrez/felipe/augusto/pkg743530/Jogador.java
@@ -1,36 +1,46 @@
 package xadrez.felipe.augusto.pkg743530;
 
-import xadrez.felipe.augusto.pkg743530.Peças.Bispo;
-import xadrez.felipe.augusto.pkg743530.Peças.Cavalo;
-import xadrez.felipe.augusto.pkg743530.Peças.Dama;
-import xadrez.felipe.augusto.pkg743530.Peças.Peao;
-import xadrez.felipe.augusto.pkg743530.Peças.Rei;
-import xadrez.felipe.augusto.pkg743530.Peças.Torre;
-
 /**
  *
  * @author Felipe Augusto - 743530
  */
 public class Jogador {
     //Atributos
-    private String nome;
-    private Peca[] p = new Peca[16]; 
+    private final String nome; //nome do jogador
+    private Peca[] p = new Peca[16]; //conjunto de pecas do jogador
+    private final int cor; //0 para jogador com pecas branco, 1 para jogador com pecas pretas
 
     //Metodos Especiais
-    public Jogador(String nome, Peca[] p) {
-        //Setando nome do jogador
+    /**
+     *Construtor de jogador, responsável por criar o jogador com seu nome, cor e suas respectivas pecas
+     * 
+     * @param nome Nome do Jogador
+     * @param p Conjunto de pecas do xadrez iniciais
+     * @param cor Cor do Jogador de acordo com sua peca
+     */
+    public Jogador(String nome, Peca[] p, int cor) {
+        //Setando nome e cor do jogador
         this.nome = nome;
-        
+        this.cor = cor;
         //Copiando Pecas para jogador
         System.arraycopy(p, 0, this.p, 0, 16);
     }
 
-    public void setP(Peca[] p) {
-        this.p = p;
+    /**
+     *Responsavel por retornar a cor do jogador
+     * 
+     * @return Retorna um inteiro
+     */
+    public int getCor() {
+        return cor;
     }
-    
-    //Metodos Personalizados
-        public boolean getstatusPeca(Peca p){
-        return p.isInGame();
+
+    /**
+     *Responsavel por retornar o nome do jogador
+     * 
+     * @return Retorna uma string
+     */
+    public String getNome() {
+        return nome;
     }
 }
