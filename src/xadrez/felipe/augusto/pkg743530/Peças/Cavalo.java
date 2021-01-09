06932f63b1931845219bfad1afde17cac03cd558
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2021-01-08 22:53:37 -0300

    feat: Cavalo completo

--- src/xadrez/felipe/augusto/pkg743530/Peças/Cavalo.java
+++ src/xadrez/felipe/augusto/pkg743530/Peças/Cavalo.java
@@ -7,10 +7,21 @@ import xadrez.felipe.augusto.pkg743530.Peca;
  */
 public class Cavalo extends Peca{
     //Metodos Especiais
+    /**
+     *Construtor de peca cavalo, responsavel por criar a peca com sua cor
+     * 
+     * @param cor Recebe a cor da peca 
+     */
     public Cavalo(int cor) {
         super(cor); //Chamando construtor herdado da classe Peca
     }
 
+    //Metodos Personalizados
+    /**
+     *Responsavel por retornar o desenho da peca de acordo com a cor dela
+     * 
+     * @return Retorna uma string que representa a peca
+     */
     @Override
     public String desenho(){
         if(getCor() == 0) //peca branca
@@ -19,6 +30,15 @@ public class Cavalo extends Peca{
             return("C-");
     }
     
+    /**
+     *Responsavel por verificar a movimentacao de acordo com as regras do xadrez
+     * 
+     * @param linhaOrigem Coordenada linha de origem da peca
+     * @param colunaOrigem Coordenada coluna de origem da peca
+     * @param linhaDestino Coordenada linha de destino da peca
+     * @param colunaDestino Coordenada linha de destino da peca
+     * @return Retorna um boolean se o movimento eh valido ou nao 
+     */
     @Override
     public boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino){
         switch (Math.abs(linhaDestino - linhaOrigem)) {
