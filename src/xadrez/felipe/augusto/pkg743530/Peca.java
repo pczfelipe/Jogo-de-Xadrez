6e0161f6da4119fd3a214d9dd02891a5cba62032
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2021-01-08 22:53:56 -0300

    feat: Peca completo

--- src/xadrez/felipe/augusto/pkg743530/Peca.java
+++ src/xadrez/felipe/augusto/pkg743530/Peca.java
@@ -10,24 +10,58 @@ public abstract class Peca {
     protected boolean inGame; //true para em jogo, false para capturada
     
     //Metodos Especiais
+    /**
+     *Construtor de peca, responsavel por criar a peca e colocar ela em jogo
+     * 
+     * @param cor Recebe a cor da peca 
+     */
     protected Peca(int cor) {
         this.cor = cor;
         this.inGame = true; //ao construir a peca, ela ja esta em jogo
     }
 
+    /**
+     *Retorna a cor da peca
+     * 
+     * @return Retorna um inteiro representando a cor da peca
+     */
     protected int getCor() {
         return cor;
     }
 
+    /**
+     *Responsavel por retornar se a peca esta em jogo ou nao
+     * 
+     * @return Retorna um boolean 
+     */
     protected boolean isInGame() {
         return inGame;
     }
 
+    /**
+     *Responsavel por remover a peca do jogo
+     * 
+     * @param inGame Recebe status atual da peca no jogo 
+     */
     protected void setInGame(boolean inGame) {
         this.inGame = inGame;
     }
     
     //Metodos Personalizados
+    /**
+     *Responsavel por retornar o desenho da peca de acordo com a cor dela
+     * 
+     * @return Retorna uma string que representa a peca
+     */
     protected abstract String desenho();
+     /**
+     *Responsavel por verificar a movimentacao de acordo com as regras do xadrez
+     * 
+     * @param linhaOrigem Coordenada linha de origem da peca
+     * @param colunaOrigem Coordenada coluna de origem da peca
+     * @param linhaDestino Coordenada linha de destino da peca
+     * @param colunaDestino Coordenada linha de destino da peca
+     * @return Retorna um boolean se o movimento eh valido ou nao 
+     */
     protected abstract boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino);
 }
