24dee25cf68b262662bdf14b5a6818a5efac92b6
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2021-01-08 22:53:28 -0300

    feat: Dama completo

--- src/xadrez/felipe/augusto/pkg743530/Peças/Dama.java
+++ src/xadrez/felipe/augusto/pkg743530/Peças/Dama.java
@@ -7,11 +7,21 @@ import xadrez.felipe.augusto.pkg743530.Peca;
  */
 public class Dama extends Peca{
     //Metodos Especiais
+    /**
+     *Construtor de peca rainha, responsavel por criar a peca com sua cor
+     * 
+     * @param cor Recebe a cor da peca 
+     */
     public Dama(int cor) {
         super(cor); //Chamando construtor herdado da classe Peca
     }
 
     //Metodos Personalizados
+    /**
+     *Responsavel por retornar o desenho da peca de acordo com a cor dela
+     * 
+     * @return Retorna uma string que representa a peca
+     */
     @Override
     public String desenho(){
         if(getCor() == 0) //peca branca
@@ -20,6 +30,15 @@ public class Dama extends Peca{
             return("D-");
     }
     
+    /**
+     *Responsavel por verificar a movimentacao de acordo com as regras do xadrez
+     * 
+     * @param linhaOrigem Coordenada linha de origem da peca
+     * @param colunaOrigem Coordenada coluna de origem da peca
+     * @param linhaDestino Coordenada linha de destino da peca
+     * @param colunaDestino Coordenada linha de destino da peca
+     * @return Retorna um boolean se o movimento eh valido ou nao 
+     */
     @Override
     public boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino){
         return (Math.abs(linhaDestino - linhaOrigem) == Math.abs(colunaDestino - colunaOrigem)) || (linhaOrigem == linhaDestino && colunaOrigem != colunaDestino) || ((linhaOrigem != linhaDestino) && (colunaOrigem == colunaDestino));
