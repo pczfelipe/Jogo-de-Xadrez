6591838daabf6ef9bbec15b872490adf42fd3e72
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2021-01-08 22:52:23 -0300

    feat: Rei completo

--- src/xadrez/felipe/augusto/pkg743530/Peças/Rei.java
+++ src/xadrez/felipe/augusto/pkg743530/Peças/Rei.java
@@ -7,11 +7,21 @@ import xadrez.felipe.augusto.pkg743530.Peca;
  */
 public class Rei extends Peca{
     //Metodos Especiais
+    /**
+     *Construtor de peca rei, responsavel por criar a peca com sua cor
+     * 
+     * @param cor Recebe a cor da peca 
+     */
     public Rei(int cor) {
         super(cor); //Chamando construtor herdado da classe Peca
     }
 
     //Metodos Personalizados
+    /**
+     *Responsavel por retornar o desenho da peca de acordo com a cor dela
+     * 
+     * @return Retorna uma string que representa a peca
+     */
     @Override
     public String desenho(){
         if(getCor()== 0) //peca branca
@@ -20,6 +30,15 @@ public class Rei extends Peca{
             return("R-");
     }
     
+    /**
+     *Responsavel por verificar a movimentacao de acordo com as regras do xadrez
+     * 
+     * @param linhaOrigem Coordenada linha de origem da peca
+     * @param colunaOrigem Coordenada coluna de origem da peca
+     * @param linhaDestino Coordenada linha de destino da peca
+     * @param colunaDestino Coordenada linha de destino da peca
+     * @return Retorna um boolean se o movimento eh valido ou nao 
+     */
     @Override
     public boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino){
         return Math.abs(linhaDestino - linhaOrigem) <= 1 && Math.abs(colunaDestino - colunaOrigem) <= 1;
