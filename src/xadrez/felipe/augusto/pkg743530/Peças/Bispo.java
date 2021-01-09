e7bd741e2db14a1d39044e4bd60cefdc04a60e5f
Felipe Augusto <39167935+pczfelipe@users.noreply.github.com>
2021-01-08 22:53:47 -0300

    feat: Bispo completo

--- src/xadrez/felipe/augusto/pkg743530/Peças/Bispo.java
+++ src/xadrez/felipe/augusto/pkg743530/Peças/Bispo.java
@@ -7,11 +7,21 @@ import xadrez.felipe.augusto.pkg743530.Peca;
  */
 public class Bispo extends Peca{
     //Metodos Especiais
+    /**
+     *Construtor de peca bispo, responsavel por criar a peca com sua cor
+     * 
+     * @param cor Recebe a cor da peca 
+     */
     public Bispo(int cor) {
         super(cor); //Chamando construtor herdado da classe Peca
     }
 
     //Metodos Personalizados
+    /**
+     *Responsavel por retornar o desenho da peca de acordo com a cor dela
+     * 
+     * @return Retorna uma string que representa a peca
+     */
     @Override
     public String desenho(){
         if(getCor() == 0) //peca branca
@@ -20,6 +30,15 @@ public class Bispo extends Peca{
             return("B-");
     }
     
+    /**
+     *Responsavel por verificar a movimentacao de acordo com as regras do xadrez
+     * 
+     * @param linhaOrigem Coordenada linha de origem da peca
+     * @param colunaOrigem Coordenada coluna de origem da peca
+     * @param linhaDestino Coordenada linha de destino da peca
+     * @param colunaDestino Coordenada linha de destino da peca
+     * @return Retorna um boolean se o movimento eh valido ou nao 
+     */
     @Override
     public boolean checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino){
         return Math.abs(linhaDestino - linhaOrigem) == Math.abs(colunaDestino - colunaOrigem);
